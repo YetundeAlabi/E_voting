053e158344ce2ff25bcaf620f201522f87626185
YetundeAlabi <alabiyetunde007@gmail.com>
2023-05-03 12:36:49 +0100

    admin register models

--- e_voting/admin.py
+++ e_voting/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from accounts.models import Voter
+from e_voting.models import Poll, Candidate, Vote
 # Register your models here.
+admin.site.register(Poll)
+admin.site.register(Candidate)
+admin.site.register(Vote)
+admin.site.register(Voter)
