f7090463983b9101d5ade2b5538c5164958603b3
YetundeAlabi <alabiyetunde007@gmail.com>
2023-05-03 12:00:29 +0100

    modified poll name and added absolute url

--- e_voting/models.py
+++ e_voting/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse
 from accounts.models import User, Voter
 
 # Create your models here.
-class VotingPoll(models.Model):
+class Poll(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField(null=True)
     start_time = models.DateTimeField(auto_now_add=True)
@@ -11,21 +12,29 @@ class VotingPoll(models.Model):
     updated_end_time = models.DateTimeField(auto_now = True)
     is_deleted = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
-
+    
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse("poll_detail", kwargs={"pk": self.pk})
+    
 
 
 class Candidate(models.Model):
     name = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="/images", null=True)
+    image = models.ImageField(upload_to="e_voting/candidates", null=True)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE )
 
     def __str__(self):
         return self.name
     
-
+    def get_absolute_url(self):
+        return reverse("candidate_detail", kwargs={"pk": self.pk})
+    
+    
 class Vote(models.Model):
-    poll = models.ForeignKey(VotingPoll, on_delete=models.CASCADE)
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
     choice = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     voted_by = models.ForeignKey(Voter, on_delete=models.CASCADE)
 
