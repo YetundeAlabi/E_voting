df69a9a433877396064c251563b8f7e794119829
YetundeAlabi <alabiyetunde007@gmail.com>
2023-05-16 09:16:49 +0100

    removed static

--- polls/settings.py
+++ polls/settings.py
@@ -52,9 +52,9 @@ INSTALLED_APPS = [
     "accounts",
     "api",
     "phonenumber_field",
-    "frontend",
-    "myapp",
-    "voting"
+    # "frontend",
+    # "myapp",
+    # "voting"
 ]
 
 MIDDLEWARE = [
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 
