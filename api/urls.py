c8b95fef25e02bc077c7d279e23359d1c0bab55e
Yetunde Alabi <alabiyetunde007@gmail.com>
2023-05-16 07:33:58 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 from api import views
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Documentation",
+      default_version='v1',
+      description="Voting App",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@e_voting.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+   
 urlpatterns = [
     path('polls/', views.PollListCreateView.as_view(), name='polls'),
     path('polls/<int:pk>/', views.PollDetailView.as_view(), name='poll_detail'),
@@ -13,5 +30,6 @@ urlpatterns = [
     path('polls/<int:pk>/result/', views.PollResultView.as_view(), name='poll_result'),
     path('voters/', views.VoterListView.as_view(), name='voter_list'),
     path('polls/<int:pk>/voters/<int:voter_pk>/', views.VoterDetailView.as_view(), name="voter_detail"),
-    path("send-email/", views.SendPollEmailView.as_view(), name="send-email")
+    path("send-email/", views.SendPollEmailView.as_view(), name="send-email"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
